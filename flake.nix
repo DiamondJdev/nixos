51f6d3c51a3f7428cee8e75b49d7d0584febb4fe
diamondjdev <diamondjdev@gmail.com>
2026-08-23 00:47:09 -0500

    renabled ssh

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
         modules = [
           ./configuration.nix
           ./hardware-configuration.nix
-          # ./modules/ssh.nix # Commented as to disable for now
+          ./modules/ssh.nix # Commented as to disable for now
           ./modules/swap.nix
         ];
       };
