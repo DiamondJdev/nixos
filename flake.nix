385a549b42e74929f6e77f3262335a9c45e7aaa2
diamondjdev <diamondjdev@gmail.com>
2026-08-24 20:29:25 -0500

    switch home-manager version

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     zen-browser.url = "github:youwen5/zen-browser-flake";
     hyprland.url = "github:hyprwm/Hyprland";
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
